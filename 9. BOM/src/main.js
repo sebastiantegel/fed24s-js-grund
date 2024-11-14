669f230790183d33f2a44a7b90d3d98a71f59189
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2024-11-14 10:45:47 +0100

    Implemented fetch from omdb

--- 9. BOM/src/main.js
+++ 9. BOM/src/main.js
@@ -43,3 +43,28 @@ document.getElementById("watch").addEventListener("click", () => {
 document.getElementById("clear").addEventListener("click", () => {
   navigator.geolocation.clearWatch(watchId);
 });
+
+document.getElementById("getmovies").addEventListener("click", async () => {
+  const response = await fetch("https://omdbapi.com/?apikey=416ed51a&s=harry");
+  const result = await response.json();
+
+  console.log(result.Search);
+
+  const moviesContainer = document.getElementById("movies");
+
+  for (let i = 0; i < result.Search.length; i++) {
+    const container = document.createElement("div");
+    const title = document.createElement("h3");
+    const img = document.createElement("img");
+
+    container.className = "movie";
+    title.innerHTML = result.Search[i].Title;
+    img.src = result.Search[i].Poster;
+    img.alt = result.Search[i].Title;
+
+    container.appendChild(title);
+    container.appendChild(img);
+
+    moviesContainer.appendChild(container);
+  }
+});
