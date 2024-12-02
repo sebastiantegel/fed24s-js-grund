6cf445f989cbaf417bc693d651326a75c9c3e695
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2024-12-02 10:38:11 +0100

    Implemented typescript in DOM

--- TS/2. DOM/src/main.ts
+++ TS/2. DOM/src/main.ts
@@ -1,1 +1,47 @@
+import { Todo } from "./models/Todo";
 import "./style.css";
+
+// const theForm = document.getElementById("todoForm");
+// if(theForm) {
+//   theForm.addEventListener("submit", () => {})
+// }
+
+document.getElementById("todoForm")?.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const theInputTag = document.getElementById("todoText");
+
+  if (theInputTag) {
+    const theText = (theInputTag as HTMLInputElement).value;
+    todos.push(new Todo(theText));
+    createHtml();
+  }
+});
+
+const todo = new Todo("Lorem");
+const todos: Todo[] = [todo];
+
+const ul = document.getElementById("todos");
+
+const createHtml = () => {
+  if (ul) {
+    ul.innerHTML = "";
+  }
+
+  todos.forEach((t) => {
+    const container = document.createElement("li");
+    const span = document.createElement("span");
+
+    span.innerHTML = t.text;
+
+    container.appendChild(span);
+
+    // if (ul) {
+    //   ul.appendChild(container);
+    // }
+
+    ul?.appendChild(container);
+  });
+};
+
+createHtml();
